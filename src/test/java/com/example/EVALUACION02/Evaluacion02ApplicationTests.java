e0e2b9044b5cb39068f81c9095880f49dbf25f8a
FrankJrr31 <134321340+FrankJrr31@users.noreply.github.com>
2023-07-03 21:42:32 +0000

    nuevo

---
+++ src/test/java/com/example/EVALUACION02/Evaluacion02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EVALUACION02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
